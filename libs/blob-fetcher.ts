560a34f9b26af45e568f7e0e303d32cda590a7ff
katriel <katriel.developer@gmail.com>
2024-05-06 14:40:31 -0300

    t1

---
+++ libs/blob-fetcher.ts
@@ -0,0 +1,19 @@
+console.log(import.meta.env)
+
+// import { getStore } from "@netlify/blobs";
+
+// const gallery = getStore("test");
+
+// let blobCount = 0;
+
+// console.log(await gallery.list({ paginate: true }))
+
+
+// for await (const entry of gallery.list({ paginate: true })) {
+// 	console.log(entry)
+// }
+
+console.log('------------------------')
+
+// const path = "./file.txt";
+// Bun.write(path, "Lorem ipsum");
