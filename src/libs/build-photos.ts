0ee1e9259e8449efedd2a5432660707cd3bfacb8
katriel <katriel.developer@gmail.com>
2024-05-07 20:34:25 -0300

    test deploy

---
+++ src/libs/build-photos.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import { getStore } from "@netlify/blobs";
+
+export async function generatePhotos() {
+	const gallery = getStore({
+		name: "gallery", 
+		siteID: import.meta.env.SITE_ID,
+		token: import.meta.env.SNAPI_TOKEN
+	});
+
+	const { blobs } = await gallery.list();
+	
+	// Generate photos from blobs
+	for (const { key } of blobs) {
+		const { data, metadata } = await gallery.getWithMetadata(key, {
+			type: "arrayBuffer",
+		});
+		console.log(`Created ${key}.${metadata.ext} successfully!`);
+
+		// Can also use new Uint8Array() instead of Buffer.from();
+		fs.writeFile(`./src/gallery/${key}.${metadata.ext }`, Buffer.from(data), (err) => {
+			if (err) throw err;
+			console.log(`Created ${key}.${metadata.ext} successfully!`);
+		});
+	}
+}
