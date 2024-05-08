0aa7b882a378ec33b0bbafc437f12f6b21bb5b90
katriel <katriel.developer@gmail.com>
2024-05-08 08:57:36 -0300

    test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: netlify({
     edgeMiddleware: true,
-    imageCDN: import.meta.env.PROD,
-  })
+    imageCDN: import.meta.env.PROD
+  }),
+  integrations: [
+    icon({ include: { tabler: ['*'] }})
+  ]
 });
