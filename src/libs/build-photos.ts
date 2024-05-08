2529075c63b8847b647201d632bd9fec35959700
katriel <katriel.developer@gmail.com>
2024-05-08 14:46:14 -0300

    test deploy

--- src/libs/build-photos.ts
+++ src/libs/build-photos.ts
@@ -1,29 +1,31 @@
 import fs from "fs";
 import { getStore } from "@netlify/blobs";
+import path from "path";
 
 export async function generatePhotos() {
 	console.log("Generating photos...");
 
 	const gallery = getStore({
-		name: "gallery", 
+		name: "gallery",
 		siteID: import.meta.env.SITE_ID,
 		token: import.meta.env.SNAPI_TOKEN
 	});
 
 	const { blobs } = await gallery.list();
 	console.log(await gallery.list())
-	
+
 	// Generate photos from blobs
 	for (const { key } of blobs) {
 		const { data, metadata } = await gallery.getWithMetadata(key, {
 			type: "arrayBuffer",
 		});
 
-		// Can also use new Uint8Array() instead of Buffer.from();
-		await fs.promises.writeFile(`./_astro/${key}.${metadata.ext}`, Buffer.from(data))
+		const filePath = path.join(__dirname, '..', 'gallery', `${key}.${metadata.ext}`);
+		
+		await fs.promises.writeFile(filePath, Buffer.from(data))
 			.then(() => console.log(`Created ${key}.${metadata.ext} successfully!`))
 			.catch((err) => console.error(`Error generating ${key}.${metadata.ext}: ${err.message}`))
 	}
 }
 
-await generatePhotos()
+await generatePhotos();
