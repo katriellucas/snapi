638cd8da23a12aecb113161605a7d119df5cc2de
katriel <katriel.developer@gmail.com>
2024-05-11 16:27:55 -0300

    test deploy

--- src/libs/session.ts
+++ src/libs/session.ts
@@ -1,10 +1,10 @@
 import { user } from '../libs/stores';
 
-export async function validSession(session: string | undefined) {
+export async function validSession(session: string | undefined): Promise<boolean> {
 	try {
 		console.error("Getting session from blob store:");
 		const token = await user.get('session');
-		return session && session === token;
+		return (session !== undefined) && (session === token);
 	} catch (error) {
 		console.error("Error validating session:", error);
 		return false;
