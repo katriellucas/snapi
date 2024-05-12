f23b5cad176b563905abf56432599dc7e9ebfe58
katriel <katriel.developer@gmail.com>
2024-05-12 05:53:19 -0300

    test deploy

--- src/libs/forms.ts
+++ src/libs/forms.ts
@@ -3,7 +3,9 @@ import { gallery, user } from '../libs/stores';
 
 export async function uploadPhotoForm(formData: any) {
 	try {
+		console.log(formData);
 		const file = formData.get('file') as File;
+		const alt = formData.get('alt');
 		const uuid = Date.now().toString();
 
 		// Check if photo already exists
@@ -18,7 +20,12 @@ export async function uploadPhotoForm(formData: any) {
 			return 'Invalid file type.';
 		}
 
-		await gallery.set(uuid, file, { metadata: fileType });
+		await gallery.set(uuid, file, { metadata: {...fileType, alt} });
+
+		const profile = await user.get('profile', { type: 'json' });
+		await user.setJSON('profile', { ...profile, has_updates: true });
+
+		return 'Photo uploaded successfully!';
 	} catch (error) {
 		console.error(error);
 		return 'Some error ocurred.';
@@ -33,8 +40,9 @@ export async function profileUpdateForm(formData: any) {
 		const action_label = formData.get('action-label');
 		const action_link = formData.get('action-label');
 
-		await user.setJSON('profile', { name, job, description, action_label, action_link });
+		await user.setJSON('profile', { name, job, description, action_label, action_link, has_updates: true });
 
+		return 'Profile updated successfully!';
 	} catch (error) {
 		console.error(error);
 		return 'Some error ocurred.';
@@ -52,10 +60,12 @@ export async function deployForm() {
 	};
 
 	// Send the fetch request
-	fetch(buildHookUrl, options)
-		.then(response => {
-			if (!response.ok) throw new Error('Failed to trigger build hook');
-			console.log('Build hook triggered successfully');
+	return await fetch(buildHookUrl, options)
+		.then(async response => {
+			if (!response.ok) return 'Failed to trigger build hook';
+			const profile = await user.get('profile', { type: 'json' });
+			await user.setJSON('profile', { ...profile, has_updates: false });
+			return 'Build hook triggered successfully, your Snapi will be deployed soon';
 		})
-		.catch(error => console.error('Error triggering build hook:', error.message))
+		.catch(error => 'Error triggering build hook: ' + error.message)
 }
