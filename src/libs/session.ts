c0b9483f302230f7e5efdc97b3787d7cf6633190
katriel <katriel.developer@gmail.com>
2024-05-12 23:04:16 -0300

    test deploy

--- src/libs/session.ts
+++ src/libs/session.ts
@@ -2,9 +2,9 @@ import { user } from '../libs/stores';
 
 export async function validSession(session: string | undefined): Promise<boolean> {
 	const token = await user.get('session');
-
-	if (session) {
-		return session === token;
+	console.log(token, session);
+	if (session && session === token) {
+		return true;
 	} else {
 		return false;
 	}
